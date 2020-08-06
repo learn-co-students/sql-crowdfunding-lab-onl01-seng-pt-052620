592264d100a890a2f1d50bd871b9cb72305442c3
rumikoacopa <rumikorios@gmail.com>
2020-08-06 13:12:40 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (id, name, age)
+VALUES (1, 'Eddie', 32), (2, 'Kira', 15), (3, 'Will', 13), (4, 'Rumiko', 35), (5, 'Sunny', 34),
+(6, 'Karen', 30), (7, 'Aslan', 14), (8, 'Kay', 40), (9, 'Monica', 28), (10, 'Samantha', 29), (11, 'Delaney', 10),
+(12, 'Gates', 14), (13, 'Kay', 41) (14, 'Jason', 40), (15, 'Songja', 63), (16, 'Miko', 25), (17, 'Junior', 18),
+(18, 'Aric', 21), (19, 'Carina', 22), (20, 'Martin', 20);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (1, 'Harry Potter', 'Magic', 32000, '08/30/2020', '02/20/2022'), 
+(2, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(3, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(4, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(5, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(6, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(7, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(8, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(9, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02'),
+(10, 'Harry Potter', 'Magic', 32000, '2020-30-08', '2022-20-02');
